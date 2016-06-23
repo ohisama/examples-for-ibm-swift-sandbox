a9fec569fb02e0330c416e892c2cb57d62c7d4d2
yuki <yuki@android>
2016-06-23 11:27:37 +0900

    test0.19

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "examples-for-ibm-swift-sandbox",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 16),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 19),
     ]
 )
