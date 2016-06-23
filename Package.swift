c0441b9ec92efa60ad46d2f6d553cafe0448fd13
yuki <yuki@android>
2016-06-23 10:49:20 +0900

    test2

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "examples-for-ibm-swift-sandbox",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 8),
+        //.Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 8),
         //.Package(url: "https://github.com/IBM-Swift/BlueCryptor.git", majorVersion: 0, minor: 2),
         //.Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 9),
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 19),
