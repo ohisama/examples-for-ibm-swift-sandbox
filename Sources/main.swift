33c655875405d8666f833c7652fe78a10cd44ca6
yuki <yuki@android>
2016-06-23 10:29:07 +0900

    kitura

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,97 +1,43 @@
-/*
-    Please choose `Swift Ver. 3.0 (May 9, 2016)` version
-    and
-    Target: x86_64-ubuntu-linux-gnu
 
-    See here:
-    https://github.com/naokits/examples-for-ibm-swift-sandbox/tree/master
- */
-
-
+import KituraNet
+import Dispatch
 import Foundation
-
-print("\r\n--------------------------------------------")
-print("SwiftyJSON")
-print("--------------------------------------------")
-
 import SwiftyJSON
 
-func JSONExample() {
-    let json = JSON(["name":"naokits", "age": 50])
-
-    if let name = json["name"].string {
-        print(name)
+func main(args:[String:Any]) -> [String:Any] {
+    var str = "No response"
+    dispatch_sync(dispatch_get_global_queue(0, 0)) {
+        HTTP.get("https://httpbin.org/get") { response in
+            do 
+            {
+                str = try response!.readString()!
+            } 
+            catch 
+            {
+                print("Error \(error)")
+            }
+        }
     }
-
-    if let age = json["age"].int {
-        print(age)
+    print("Got string \(str)")
+    var result:[String:Any]?
+    let data = str.data(using: NSUTF8StringEncoding, allowLossyConversion: true)!
+    let json = JSON(data: data)
+    if let jsonUrl = json["url"].string 
+    {
+        print("Got json url \(jsonUrl)")
     }
+    else 
+    {
+        print("JSON DID NOT PARSE")
+    }
+    do 
+    {
+        result = try NSJSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+    }
+    catch 
+    {
+        print("Error \(error)")
+    }
+    print("Result is \(result!)")
+    return result!
 }
-
-JSONExample()
-
-print("\r\n--------------------------------------------")
-print("Cryptor")
-print("--------------------------------------------")
-
-import Cryptor
-
-let key = CryptoUtils.byteArray(fromHex: "2b7e151628aed2a6abf7158809cf4f3c")
-let iv = CryptoUtils.byteArray(fromHex: "00000000000000000000000000000000")
-let plainText = CryptoUtils.byteArray(fromHex: "6bc1bee22e409f96e93d7e117393172a")
-print("Plaintext  = " + CryptoUtils.hexString(from: plainText))
-
-var textToCipher = plainText
-if plainText.count % Cryptor.Algorithm.aes.blockSize != 0 {
-    textToCipher = CryptoUtils.zeroPad(byteArray: plainText, blockSize: Cryptor.Algorithm.aes.blockSize)
-}
-let cipherText = Cryptor(operation: .encrypt, algorithm: .aes, options: .none, key: key, iv: iv).update(byteArray: textToCipher)?.final()
-print("Ciphertext = " + CryptoUtils.hexString(from: cipherText!))
-
-let decryptedText = Cryptor(operation: .decrypt, algorithm: .aes, options: .none, key: key, iv: iv).update(byteArray: cipherText!)?.final()
-print("Decrypted  = " + CryptoUtils.hexString(from: decryptedText!))
-
-
-print("\r\n--------------------------------------------")
-print("HeliumLogger")
-print("--------------------------------------------")
-
-import HeliumLogger
-import LoggerAPI
-
-let logger = HeliumLogger()
-Log.logger = logger
-
-Log.verbose("This is a verbose log message.")
-
-Log.info("This is an informational log message.")
-
-Log.warning("This is a warning.")
-
-Log.error("This is an error.")
-
-Log.debug("This is a debug message.")
-
-let type = LoggerMessageType.info
-logger.log(type, msg: "This a dynamic message", functionName: "no func", lineNum: #line, fileName: #file)
-
-/*
-
-print("\r\n--------------------------------------------")
-print("Kitura")
-print("--------------------------------------------")
-
-import Kitura
-
-let router = Router()
-
-router.get("/") {
-request, response, next in
-    response.send("Hello, World!")
-    next()
-}
-
-Kitura.addHTTPServer(onPort: 8090, with: router)
-Kitura.run()
-
-*/
